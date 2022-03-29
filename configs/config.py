e587825f254fec090f877af4c9ae862ddf5f2516
Morgan-Gicheha <morgangicheha4@gmail.com>
2022-03-29 06:56:50 +0300

    env configs

--- configs/config.py
+++ configs/config.py
@@ -7,8 +7,8 @@ class DevelopmentConfig:
     JWT_SECRET_KEY = os.urandom(20)
     # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:morgan@127.0.0.1:5432/restx"
     # SQLALCHEMY_DATABASE_URI = "sqlite:///restx.db"
-    SQLALCHEMY_DATABASE_URI = "postgres://kbmithtlwhakxe:024ee18efebbd5e0d391714a28046b216c933f2750974f46e1b89e1f8f160ab5@ec2-34-235-33-44.compute-1.amazonaws.com:5432/d2r68bip0nkt1c"
-    
+ 
+# 
     
 
 
