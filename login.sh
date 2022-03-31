020842fecbf5f57e8a528cd3e52015f3e000224a
Morgan-Gicheha <morgangicheha4@gmail.com>
2022-04-01 01:28:53 +0300

    shell script run trial 25

--- login.sh
+++ login.sh
@@ -4,10 +4,15 @@ HOSTS="138.68.189.32"
 SCRIPT="pwd"
 value=$(<travisDeploy)
 for HOSTNAME in ${HOSTS} ; do
-    eval `ssh-agent -s`
-    # ssh-add -T pubkey ${value}
-    ssh -o StrictHostKeyChecking=no -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
-    # ping google.com
+    ssh -i ./travisDeploy root@138.68.189.32 'ls'
+    
+    # eval `ssh-agent -s`
+    # # ssh-add -T pubkey ${value}
+    # # ssh -o StrictHostKeyChecking=no -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
+    # ssh  root@138.68.189.32
+
+
 done
 
-ssh root@138.68.189.32
+
+
