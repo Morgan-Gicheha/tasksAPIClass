a0bb13ff82a1d7329eaf7c906a3722b16d37fadf
Morgan-Gicheha <morgangicheha4@gmail.com>
2022-03-31 09:02:40 +0300

    shell script run trial 15

--- login.sh
+++ login.sh
@@ -3,6 +3,6 @@ HOSTS="138.68.189.32"
 SCRIPT="pwd"
 for HOSTNAME in ${HOSTS} ; do
     # eval `ssh-agent -s`
-    # ssh -o StrictHostKeyChecking=no -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
+    ssh -o StrictHostKeyChecking=no -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
     ping  ${HOSTNAME}
 done
