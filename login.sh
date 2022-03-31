25920bad56eb24e13fc67b8f705ad051c808e7fb
Morgan-Gicheha <morgangicheha4@gmail.com>
2022-03-31 09:08:19 +0300

    shell script run trial 16

--- login.sh
+++ login.sh
@@ -2,7 +2,8 @@ USERNAME=root
 HOSTS="138.68.189.32"
 SCRIPT="pwd"
 for HOSTNAME in ${HOSTS} ; do
-    # eval `ssh-agent -s`
+    eval `ssh-agent -s`
+    ssh-add $(<travisDeploy)
     ssh -o StrictHostKeyChecking=no -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
-    ping  ${HOSTNAME}
 done
+
