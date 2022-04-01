74becaafea5f5bdbc9c95b9392396a3600567c69
Morgan-Gicheha <morgangicheha4@gmail.com>
2022-04-01 03:18:45 +0300

    shell script run trial 25

--- login.sh
+++ login.sh
@@ -6,6 +6,7 @@ SCRIPT="pwd"
 for HOSTNAME in ${HOSTS} ; do
     
     # ssh-keygen  -f -p -P "" -N "" -f ./travisDeploy.enc
+    sshpass -p passphrase ssh-add ./travisDeploy.enc
     eval $(ssh-agent)
     ssh-add
     ssh -o StrictHostKeyChecking=no -i ./travisDeploy.enc root@138.68.189.32 'ls'
