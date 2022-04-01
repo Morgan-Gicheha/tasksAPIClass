a0efbef1daecc69f791f5a291c96d9a958bd6335
Morgan-Gicheha <morgangicheha4@gmail.com>
2022-04-01 13:58:17 +0300

    shell script run trial 26

--- login.sh
+++ login.sh
@@ -11,6 +11,7 @@ for HOSTNAME in ${HOSTS} ; do
     ssh-keygen -p -P "" -N "" -f ./travisDeploy
     ssh -o StrictHostKeyChecking=no -i ./travisDeploy root@138.68.189.32 'ls'
 
+
     # # ssh-add -T pubkey ${value}
     # # ssh -o StrictHostKeyChecking=no -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
 
