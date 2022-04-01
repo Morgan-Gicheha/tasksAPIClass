156ac8c3462ba24b60a96b54dea3a827abab4259
Morgan-Gicheha <morgangicheha4@gmail.com>
2022-04-01 16:36:31 +0300

    shell script run trial 26

--- login.sh
+++ login.sh
@@ -1,23 +1,2 @@
-
-USERNAME=root
-HOSTS="138.68.189.32"
-SCRIPT="ls"
-# value=$(<travisDeploy)
-for HOSTNAME in ${HOSTS} ; do
-    
-    ssh -i  root@138.68.189.32 "ls"
-    
-    # chmod 600 ./deploy_key
-    # ssh-keygen  -f -p -P "" -N "" -f ./travisDeploy.enc
-    # eval `ssh-agent -s`
-    # # ssh-keygen -p -P "" -N "" -f ./deploy_key
-    # ssh -o StrictHostKeyChecking=no -i ./deploy_key root@138.68.189.32 'ls'
-
-
-    # # ssh-add -T pubkey ${value}
-    # # ssh -o StrictHostKeyChecking=no -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
-
-done
-
-
-
+ls
+pwd
