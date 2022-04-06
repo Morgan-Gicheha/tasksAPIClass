6d79b06b440da4c3beafa34b8cac032d8f550524
Morgan-Gicheha <morgangicheha4@gmail.com>
2022-04-06 21:33:03 +0300

    shell script run trial 26

--- login.sh
+++ login.sh
@@ -2,7 +2,7 @@
 
 USERNAME=root
 HOSTS="138.68.189.32"
-SCRIPT=" cd tasksAPIClass;git pull ;docker-compose down; docker-compose -d up --build"
+SCRIPT=" cd tasksAPIClass;git pull ;docker-compose down; docker-compose up --build -d"
 for HOSTNAME in ${HOSTS} ; do
     ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
 done
