9b528bea6d3fb372b3a406708883dd2cbce703a5
Morgan-Gicheha <morgangicheha4@gmail.com>
2022-04-06 18:16:05 +0300

    .

--- login.sh
+++ login.sh
@@ -2,7 +2,7 @@
 
 USERNAME=root
 HOSTS="138.68.189.32"
-SCRIPT=" rm -r tasksAPIClass; git clone https://github.com/Morgan-Gicheha/tasksAPIClass.git; cd tasksAPIClass; ls"
+SCRIPT=" cd tasksAPIClass;git pull ; docker-compose up --build"
 for HOSTNAME in ${HOSTS} ; do
     ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
 done
