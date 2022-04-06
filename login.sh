f38456d277f8f862091f7e1d96c46bb6f4caf456
Morgan-Gicheha <morgangicheha4@gmail.com>
2022-04-06 16:13:38 +0300

    .

--- login.sh
+++ login.sh
@@ -2,7 +2,7 @@
 
 USERNAME=root
 HOSTS="138.68.189.32"
-SCRIPT=" rm -r tasksAPIClass; git clone git@github.com:Morgan-Gicheha/tasksAPIClass.git; cd tasksAPIClass; ls"
+SCRIPT=" rm -r tasksAPIClass; git clone https://github.com/Morgan-Gicheha/tasksAPIClass.git; cd tasksAPIClass; ls"
 for HOSTNAME in ${HOSTS} ; do
     ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
 done
