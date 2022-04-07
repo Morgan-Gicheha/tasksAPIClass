7b542a8797480f41afd5a8fc147e9c48419b6664
Morgan-Gicheha <morgangicheha4@gmail.com>
2022-04-07 15:21:17 +0300

    shell script run trial 26

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello from Morgan trial3'
+    return 'Hello from Morgan trial4 with Edwin'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
